2caccae1af25666f00d69069284c3c5c926f0aff
phillion9503 <phillion9503@gmail.com>
2016-07-20 00:02:04 +0530

    Profile Template v0.1
    Profile Template v0.1

---
+++ templates/profile/script.js
@@ -0,0 +1,11 @@
+$(function(){
+	      $(".image-container .typed").typed({
+	        strings: ["loves to code", 
+	        		  "web developer",
+	        		  "software engineer",
+	        		  "competative programmer",
+	        		  "blogger"],
+	        typeSpeed: 0.1,
+	        loop: true
+	      });
+});
